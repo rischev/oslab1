a6229c5c980a47d10689a3ac2adf6d442afb2e40
rischev <romanscherbakov.i@gmail.com>
2020-11-03 21:42:04 +0300

    seems like it works

--- oslab.c
+++ oslab.c
@@ -16,16 +16,25 @@ typedef struct _malloc_data {
 
 typedef struct _writeF_data {
   int *start;
-  int fileNumber;
   size_t block;
-  int fileSize;
+  int file;
   int tid;
   int upperBound;
 } writeF_data;
 
+typedef struct _readF_data {
+  int file;
+  int tid;
+  int *acc;
+  int *ct;
+  off_t offset;
+} readF_data;
+
 pthread_mutex_t writeMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t readMutex = PTHREAD_MUTEX_INITIALIZER;
 void *fillMalloc(void *arg);
 void *writeFile(void *arg);
+void *readFile(void * arg);
 
 int main() {
   const int A = 266;
@@ -68,27 +77,44 @@ int main() {
 
     pthread_t writeF[numOfFiles];
     writeF_data writeF_data[numOfFiles];
+    pthread_t readF[numOfFiles];
+    readF_data readF_data[numOfFiles];
     int tid;
-    int files[5];
+    int files[numOfFiles];
+    int allSum = 0, allCt = 1;
 
-    for (int f = 0; f < 5; f++) {
+    for (int f = 0; f < numOfFiles; f++) {
       char *name = (char *) malloc(sizeof(char));
       sprintf(name, "%i", f);
       int file = open(name, O_RDWR | O_APPEND | O_CREAT | O_DSYNC, 0644);
       free(name);
       files[f] = file;
+
+      writeF_data[f].file = files[f];
+      writeF_data[f].start = start;
+      writeF_data[f].block = (size_t) G;
+      writeF_data[f].upperBound = numOfInts;
+
+      readF_data[f].file = files[f];
+      readF_data[f].acc = &allSum;
+      readF_data[f].ct = &allCt;
+      readF_data[f].offset = (off_t) G;
     }
 
     for (int j = 0; j < Ebytes / G; j++) {
-      for (int i = 0; i < 5; i++) {
-        writeF_data[i].start = start;
-        writeF_data[i].fileNumber = i;
-        writeF_data[i].block = (size_t) G;
-        writeF_data[i].fileSize = 1050;
+      for (int i = 0; i < numOfFiles; i++) {
         writeF_data[i].tid = pthread_create(&writeF[i], NULL, writeFile, &writeF_data[i]);
-        writeF_data[i].upperBound = numOfInts;
         pthread_join(writeF[i], NULL);
+        readF_data[i].tid = pthread_create(&readF[i], NULL, readFile, &readF_data[i]);
+        pthread_join(readF[i], NULL);
       }
+      if (j % 1000 == 0) {
+        printf("Current acc = %i, ct = %i\n", allSum, allCt);
+      }
+    }
+
+    for (int f = 0; f < numOfFiles; f++) {
+      close(files[f]);
     }
 
 
@@ -116,27 +142,36 @@ void *fillMalloc(void *arg) {
 void *writeFile(void *arg) {
   pthread_mutex_lock(&writeMutex);
   writeF_data *data = (writeF_data *) arg;
-  char *name = (char *) malloc(sizeof(char));
-  sprintf(name, "%i", data->fileNumber);
-  int file = open(name, O_WRONLY | O_APPEND | O_CREAT | O_DSYNC, 0644);
-  free(name);
-
   int random, result;
   int* buf = NULL;
-  int loop = (int)( (data->fileSize) / (data->block) );
-
-  for (int i = 0; i < loop; i++) {
-    buf = data->start + rand() % data->upperBound - data->block;
-    result = write(file, buf, data->block);
-    if (result == -1) {
-      printf("Thread no.%i Write failed: %s\n", data->tid, strerror(errno));
-      close(file);
-      pthread_mutex_unlock(&writeMutex);
-      pthread_exit(NULL);
-    }
+  buf = data->start + rand() % data->upperBound - data->block;
+  result = write(data->file, buf, data->block);
+  if (result == -1) {
+    printf("Write thread no.%i failed: %s\n", data->tid, strerror(errno));
+    pthread_mutex_unlock(&writeMutex);
+    pthread_exit(NULL);
   }
-  printf("Wrote data to file no.%i\n", data->fileNumber);
-  close(file);
   pthread_mutex_unlock(&writeMutex);
+  pthread_exit(NULL);
+}
+
+void *readFile(void *arg) {
+  pthread_mutex_lock(&readMutex);
+  readF_data *data = (readF_data *) arg;
+  char* buf = (char *) malloc(data->offset);
+  lseek(data->file, -(data->offset), SEEK_END);
+  int result = read(data->file, buf, data->offset);
+  if (result == -1) {
+    printf("Read thread no.%i failed: %s\n", data->tid, strerror(errno));
+    pthread_mutex_unlock(&readMutex);
+    pthread_exit(NULL);
+    free(buf);
+  }
+  for (int i = 0; i < data->offset; i++) {
+    *(data->acc) += (int)buf[i];
+    *(data->ct) += 1;
+  }
+  free(buf);
+  pthread_mutex_unlock(&readMutex);
   pthread_exit(NULL);
 }
